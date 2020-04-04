ee3a1bb9eee2bd279e2e3ac8a5874ddf347cbba4
Karthikeyan Palaniswamy <karthikeyan.forethought@gmail.com>
2020-04-04 20:59:23 +0200

    use spread operator to retrieve all properties of state.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -7,19 +7,19 @@ const initialValue = {
 const allReducers = (state = initialValue, action)=>{
     switch (action.type) {
         case 'BLUE':
-            return { bgColor : '#00008B', pillWindowOn : state.pillWindowOn, isInitialOn : state.isInitialOn }
+            return { ...state, bgColor : '#00008B' };
         case 'RED':
-            return { bgColor : 'red', pillWindowOn : state.pillWindowOn, isInitialOn : state.isInitialOn }
-        case 'PILL_WINDOW_ON':
-            return { pillWindowOn : true, bgColor : state.bgColor, isInitialOn : state.isInitialOn };
-        case 'PILL_WINDOW_OFF':
-            return { pillWindowOn : false, bgColor : state.bgColor, isInitialOn : state.isInitialOn };
-        case 'INITIAL_WINDOW_OFF':
-            return { pillWindowOn : state.pillWindowOn, bgColor : state.bgColor, isInitialOn : false }
-        case 'INITIAL_WINDOW_ON':
-            return { pillWindowOn : state.pillWindowOn, bgColor : state.bgColor, isInitialOn : true }
+            return { ...state, bgColor : 'red' };
         case 'REVERT_BACK_COLOR':
-            return { bgColor : '#00877C', pillWindowOn : state.pillWindowOn, isInitialOn : state.isInitialOn }
+            return { ...state, bgColor : '#00877C' };
+        case 'PILL_WINDOW_ON':
+            return { ...state, pillWindowOn : true };
+        case 'PILL_WINDOW_OFF':
+            return { ...state, pillWindowOn : false };
+        case 'INITIAL_WINDOW_OFF':
+            return { ...state, isInitialOn : false };
+        case 'INITIAL_WINDOW_ON':
+            return { ...state, isInitialOn : true };
         default:
             return state
     }
